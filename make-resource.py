1124b207824f81c1d4688400214b0ce5ec999824
Prince Gupta <jagannatharjun11@gmail.com>
2019-08-19 20:06:43 +0530

    new

--- make-resource.py
+++ make-resource.py
@@ -3,6 +3,7 @@ import glob
 import argparse
 import os.path
 import sys
+import fnmatch
 
 parser = argparse.ArgumentParser(description='helper to create qbtthemes')
 parser.add_argument('-o', '--output', type=str, help='output qbtheme file', default='style.qbtheme')
@@ -11,6 +12,12 @@ parser.add_argument('files', metavar='file', type=str,
                     nargs='*', help='files for icons, supports glob patterns')
 args = parser.parse_args()
 
+def allFiles(glob):
+    [dir, pattern] = os.path.split(glob)
+    l = list()
+    for root, subdir, files in os.walk(dir):
+        l.extend([os.path.join(root, x) for x in files if fnmatch.fnmatch(os.path.join(root, x), pattern)])
+    return l
 
 if len(args.output) > 0 and not args.output.endswith('.qbtheme'):
     args.output += '.qbtheme'
@@ -18,11 +25,7 @@ if not len(args.output) or os.path.exists(args.output):
     if input('"{}" already exists, Do you want to overwrite?(y,n)'.format(args.output)) == 'n':
         exit()
 
-ResourceFiles = list()
-for arg in args.files:
-    for p in glob.glob(arg):
-        ResourceFiles.append('\t\t<file>' + p + '</file>\n')
-
+ResourceFiles = ['\t\t<file>' + x + '</file>\n' for x in [allFiles(y) for y in args.files]]
 ResourceFiles.append('\t\t<file alias="stylesheet.qss">' + args.style + '</file>\n')
 
 with open('resources.qrc', 'w') as rcc:
