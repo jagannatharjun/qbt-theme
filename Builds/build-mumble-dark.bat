f3c3c0a0072fceae51e170c6ca712774421b7828
jagannatharjun <jagannatharjun11@gmail.com>
2020-04-04 23:12:04 +0530

    release v1

--- Builds/build-mumble-dark.bat
+++ Builds/build-mumble-dark.bat
@@ -1,2 +1,6 @@
 set SCRIPT_DIR=mumble-theme
+set PATH=%PATH%;C:\Users\Prince\Downloads\Compressed\qtsass_0.2.2_win64;
+pushd %SCRIPT_DIR%\source
+qtsass -o ../Dark.qss Dark.scss
+popd
 make-resource.py -base-dir %SCRIPT_DIR%\ -find-files -output mumble-dark -style dark.qss 
--- Builds/build-mumble-lite.bat
+++ Builds/build-mumble-lite.bat
@@ -1,2 +1,6 @@
 set SCRIPT_DIR=mumble-theme
+set PATH=%PATH%;C:\Users\Prince\Downloads\Compressed\qtsass_0.2.2_win64;
+pushd %SCRIPT_DIR%\source
+qtsass -o ../Lite.qss Lite.scss
+popd
 make-resource.py -base-dir %SCRIPT_DIR%\ -find-files -output mumble-lite -style lite.qss 
