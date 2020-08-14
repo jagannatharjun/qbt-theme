d20ea4c63c613533cf02024bd970bf295a152318
jagannatharjun <jagannatharjun11@gmail.com>
2020-08-15 01:10:50 +0530

    add config.json to mumble dark

--- Builds/build-mumble-dark.bat
+++ Builds/build-mumble-dark.bat
@@ -3,4 +3,4 @@ set PATH=%PATH%;C:\Users\Prince\Downloads\Compressed\qtsass_0.2.2_win64;
 pushd %SCRIPT_DIR%\source
 qtsass -o ../Dark.qss Dark.scss
 popd
-make-resource.py -base-dir %SCRIPT_DIR%\ -find-files -output mumble-dark -style dark.qss 
+make-resource.py -base-dir %SCRIPT_DIR%\ -find-files -config dark-config.json -output mumble-dark -style dark.qss 
