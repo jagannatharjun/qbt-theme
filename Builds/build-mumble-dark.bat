bd172ebf4476fb0e0295f8c939bc13f441d98230
Cornel Cristea <cornelcristea@hotmail.com>
2024-05-06 14:49:22 +0300

    update comment keyword for bat

--- Builds/build-mumble-dark.bat
+++ Builds/build-mumble-dark.bat
@@ -7,7 +7,7 @@ popd
 rd /q /s mumble-icons
 mkdir mumble-icons
 python fill-icons.py nowshed #4B9CD3 mumble-icons
-# for /r icons/nowshed/ %%i in (*.svg) do powershell -Command "(gc %%~dpnxi) -replace '#4285f4', '#6a1b9a' | Out-File -encoding ASCII mumble-icons/%%~nxi"
+: for /r icons/nowshed/ %%i in (*.svg) do powershell -Command "(gc %%~dpnxi) -replace '#4285f4', '#6a1b9a' | Out-File -encoding ASCII mumble-icons/%%~nxi"
 python make-resource.py -base-dir %SCRIPT_DIR%\ -find-files -config dark-config.json -icons-dir mumble-icons -output mumble-dark-nowshed-recolored -style dark.qss 
 rd /q /s mumble-icons
 python make-resource.py -base-dir %SCRIPT_DIR%\ -find-files -config dark-config.json -icons-dir icons/nowshed -output mumble-dark-nowshed -style dark.qss 
